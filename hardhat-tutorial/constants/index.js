daa2eb9ec8b42dc102b0974770a09f5b3f11ef22
ANINDA <anindadutta1994@gmail.com>
2022-06-14 15:12:01 +0530

    upload

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,8 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x81bf575FcEc4A7694cB76936C18479dFe010c3EF";
+// const WHITELIST_CONTRACT_ADDRESS = "address-of-the-whitelist-contract";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
